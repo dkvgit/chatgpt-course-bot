bf7a27b0688c38601f166804f5e61e41b1c3e155
dkvgit <denvams@gmail.com>
2025-06-19 17:06:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,7 @@ OWNER_ID=5425101564
 RAILWAY_STATIC_URL="web-production-c31c.up.railway.app"
 
 PORT = int(os.getenv("PORT", 8080))
-
-
-
-
-
 PAID_USERS = set()
-
-
-# Все уроки
-LESSONS = {"lesson_0": {"title": "Введение в курс Нейросети без паники: простой старт с ChatGPT", "file_id": "BAACAgQAAxkBAAMDaEhKiH81kqU7n6gwyZrPvfHUxxkAAm4YAAIbBkFSitZ_6O09L_82BA"}, "lesson_1": {"title": "Урок 1: Знакомство с ChatGPT", "file_id": "BAACAgQAAxkBAAMFaEhLK6c1vLL79Q5g_mvoFAABWJAhAAJwGAACGwZBUioldIP1mCYkNgQ"}, "lesson_2": {"title": "Урок 2: Первый запуск и настройка", "file_id": "BAACAgQAAxkBAAMHaEhMcN2U21N1G4YapPigdwa3bo0AAnMYAAIbBkFSSue7-aHqqaU2BA"}, "lesson_3": {"title": "Урок 3: Повседневное использование ChatGPT", "file_id": "BAACAgQAAxkBAAMPaEhQ744iIguAsHpdWxgBfxSyazcAAncYAAIbBkFSnLB9razsga82BA"}, "lesson_4": {"title": "Урок 4: Программирование и наставничество", "file_id": "BAACAgQAAxkBAAMRaEhRm8VMDLUZNpFMrdT4mq_kLtsAAngYAAIbBkFSR9leUkrI6A02BA"}, "lesson_5": {"title": "Урок 5: Как правильно задавать запросы", "file_id": "BAACAgQAAxkBAAMTaEhSDiQPbKQGs_yrJHwwms0BwK8AAnkYAAIbBkFScwGvzfAicJw2BA"}, "lesson_6": {"title": "Урок 6: Работа с данными", "file_id": "BAACAgQAAxkBAAMVaEhUXy7zNyxd86eMLXgjhygot6QAAnwYAAIbBkFSMQWn8c-tXZk2BA"}, "lesson_7": {"title": "Урок 7: Ограничение и безопасность", "file_id": "BAACAgQAAxkBAAMXaEhUqD-bmcNx8EpG29hWYth3OKAAAn0YAAIbBkFS5SweQtSDfYQ2BA"}, "lesson_8": {"title": "Урок 8: Обратная связь", "file_id": "BAACAgQAAxkBAAIBIWhJTFeHwFR1TnuUYDdLrrk-chDkAAIWGQACGwZJUhFc-hqK-hf8NgQ"}}
-
-
-
 
 # === КНОПКИ ===
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
