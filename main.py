14ceca02eb9babd1d805a0b0ad92635cdc549437
dkvgit <denvams@gmail.com>
2025-06-19 17:37:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes, MessageHandler, filters
 import os
 from aiohttp import web
+
+# handlers
 from handlers.start import start
-from lessons_data import LESSONS
 from handlers.lessons import handle_step
 from handlers.payment import handle_payment_buttons
 from handlers.menu import menu, open_lesson, back_to_menu_handler
+from handlers.admin import grant  # ⬅️ вот он
+from lessons_data import LESSONS
+
+# utils
 from utils.access import load_paid_users, save_paid_users
-from handlers.admin import grant
-from utils.access import load_paid_users
+
 
 
 
