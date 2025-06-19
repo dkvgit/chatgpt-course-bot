ee001e9e7cec79fcd8a1b2ae58d8ca3af87d5cd6
dkvgit <denvams@gmail.com>
2025-06-19 23:39:03 +0700

    Update supabase_db.py

--- utils/supabase_db.py
+++ utils/supabase_db.py
@@ -1,11 +1,14 @@
 # utils/supabase_db.py
 import os
-from supabase import create_client, Client
+from supabase import create_client
+from supabase.lib.client_options import ClientOptions
+
+
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY, options=ClientOptions())
 
 async def add_paid_user(user_id: int):
     return await supabase.table("paid_users").upsert({"user_id": user_id}).execute()
